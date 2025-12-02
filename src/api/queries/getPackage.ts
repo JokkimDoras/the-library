faa1be188c0f24dd9c0a35aee1a420f2b964655e
Jokkim <jokkimxavier@gmail.com>
2025-12-02 14:26:10 +0530

    Delete src directory

--- src/api/queries/getPackage.ts
+++
@@ -1,7 +0,0 @@
-import type { PackageDetails } from "../types/packageDetails";
-
-export async function getPackage(name:string):Promise<PackageDetails> {
-    const res = await fetch(`https://registry.npmjs.org/${name}`);
-    const data = await res.json();
-    return data as PackageDetails;
-}
--- src/pages/Root.tsx
+++
@@ -1,9 +0,0 @@
-import Header from '../component/Header';
-import { Outlet } from 'react-router-dom';
-
-export default function Root () {
-    return <div className='container mx-auto px-20'>
-   <Header />
-   <Outlet />
-    </div>
-}
